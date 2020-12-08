53086f79d84aea914867b667433f997eacd4d617
Tynarus <github@tyn.dev>
2020-12-08 16:39:37 -0600

    Bootstrapping a base Electron window

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        height: 503,
+        width: 765,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+        }
+    });
+
+    mainWindow.loadFile(path.join(__dirname, '../index.html'));
+
+    // mainWindow.webContents.openDevTools();
+}
+
+app.on('ready', () => {
+    createWindow();
+
+    app.on('activate', () => {
+        if(BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+    app.quit();
+});
---
+++ src/preload.ts
@@ -0,0 +1,3 @@
+window.addEventListener('DOMContentLoaded', () => {
+    // DOM loaded event - window created, start your engines
+});

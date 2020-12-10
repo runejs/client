3acafd68d0e23493485a2204d850b35ee837b530
Tynarus <github@tyn.dev>
2020-12-09 20:33:48 -0600

    Title background pog

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,26 @@ import * as path from 'path';
 import { FileStore } from './file-store/file-store';
 
 
+function startUp(mainWindow: BrowserWindow): void {
+    const fileStore = new FileStore('cache');
+    fileStore.spriteStore.decodeSpritePacks();
+
+    // console.log(fileStore.spriteStore.getImage('logo'));
+
+    const titleImg = fileStore.getBinaryIndex().getArchive(0, false);
+    const bgUrl = `url(data:image/jpeg;base64,${new Buffer(titleImg.content).toString('base64')})`;
+
+    mainWindow.webContents.executeJavaScript(`
+    document.getElementById('title-background-left').style.background = '${bgUrl}';
+    document.getElementById('title-background-right').style.background = '${bgUrl}';
+    `);
+}
+
 function createWindow() {
     const mainWindow = new BrowserWindow({
         height: 503,
         width: 765,
+        //width: 1100,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
         }
@@ -14,15 +30,9 @@ function createWindow() {
 
     mainWindow.loadFile(path.join(__dirname, '../index.html'));
 
-    const fileStore = new FileStore('cache');
-    fileStore.spriteStore.decodeSpritePacks();
+    startUp(mainWindow);
 
-    console.log(fileStore.spriteStore.getImage('logo'));
-
-    const titleImg = fileStore.getBinaryIndex().getArchive(0, false);
-    console.log(titleImg.content);
-
-    // mainWindow.webContents.openDevTools();
+    //mainWindow.webContents.openDevTools();
 }
 
 app.on('ready', () => {

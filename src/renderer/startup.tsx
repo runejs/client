2b79bd38ea0f0ec10aa7aa47049b17baf08a2c3e
Tynarus <github@tyn.dev>
2021-01-31 17:04:01 -0600

    Existing User screen designs complete

--- src/renderer/startup.tsx
+++ src/renderer/startup.tsx
@@ -7,15 +7,25 @@ type StartupState = {
     logoBase64: string;
     titleBoxBase64: string;
     titleButtonBase64: string;
+    view: 'welcome' | 'existing_user' | 'new_user';
 }
 
 export default class Startup extends Component<{}, StartupState> {
 
-    componentWillMount() {
+    constructor(props) {
+        super(props);
+
+        this.openExistingUser = this.openExistingUser.bind(this);
+        this.openNewUser = this.openNewUser.bind(this);
+        this.back = this.back.bind(this);
+    }
+
+    componentWillMount(): void {
         this.setState({
             logoBase64: '',
             titleBoxBase64: '',
-            titleButtonBase64: ''
+            titleButtonBase64: '',
+            view: 'welcome'
         });
 
         ipcRenderer.on('synchronous-message', (event, args) => {
@@ -37,8 +47,68 @@ export default class Startup extends Component<{}, StartupState> {
         });
     }
 
+    openExistingUser(): void {
+        this.setState({ view: 'existing_user' });
+    };
 
-    render() {
+    openNewUser(): void {
+        this.setState({ view: 'new_user' });
+    };
+
+    back(): void {
+        this.setState({ view: 'welcome' });
+    };
+
+    render(): JSX.Element {
+        if(!this.state.titleButtonBase64 || !this.state.titleBoxBase64) {
+            return <></>;
+        }
+
+        const views: { [key: string]: JSX.Element } = {};
+
+        views['welcome'] = <>
+            <div id="welcome">
+                Welcome to RuneJS
+            </div>
+
+            <div id="title-button-container">
+                <div className="title-button" style={{
+                    background: `url('data:image/png;base64,${this.state.titleButtonBase64}')`
+                }} onClick={this.openNewUser}>New User</div>
+                <div className="title-button" style={{
+                    background: `url('data:image/png;base64,${this.state.titleButtonBase64}')`
+                }} onClick={this.openExistingUser}>Existing User</div>
+            </div>
+        </>
+
+        views['new_user'] = <>
+            New User
+        </>
+
+        views['existing_user'] = <>
+            <div id="existing-user">
+                Enter your username &amp; password.
+            </div>
+
+            <form id="credentials">
+                <label htmlFor="login-username">Username:</label>
+                <input type="text" maxLength={12} id="login-username" name="login-username"
+                       autoFocus={true} />
+
+                <label htmlFor="login-password">Password:</label>
+                <input type="password" maxLength={20} id="login-password" name="login-password"/>
+            </form>
+
+            <div id="title-button-container">
+                <div className="title-button" style={{
+                    background: `url('data:image/png;base64,${this.state.titleButtonBase64}')`
+                }}>Login</div>
+                <div className="title-button" style={{
+                    background: `url('data:image/png;base64,${this.state.titleButtonBase64}')`
+                }} onClick={this.back}>Cancel</div>
+            </div>
+        </>
+
         return (
             <div id="startup">
                 <div id="title-background">
@@ -56,18 +126,7 @@ export default class Startup extends Component<{}, StartupState> {
                 <div id="title-box" style={{
                     background: `url('data:image/png;base64,${this.state.titleBoxBase64}')`
                 }}>
-                    <div id="welcome">
-                        Welcome to RuneJS
-                    </div>
-
-                    <div id="title-button-container">
-                        <div className="title-button" style={{
-                            background: `url('data:image/png;base64,${this.state.titleButtonBase64}')`
-                        }}>New User</div>
-                        <div className="title-button" style={{
-                            background: `url('data:image/png;base64,${this.state.titleButtonBase64}')`
-                        }}>Existing User</div>
-                    </div>
+                    {views[this.state.view]}
                 </div>
             </div>
         );
